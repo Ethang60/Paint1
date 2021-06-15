9ef85b5145a0bd38e64b9f3203a782207f7f84ec
Ethang60 <85894147+Ethang60@users.noreply.github.com>
2021-06-15 09:29:42 +0100

    Up to date files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    Scanner no.Walls =new
+
+    Scanner(System.in);
+            System.out.println("Enter Number of Walls");
+    double no.Walls =(double)no.Walls.nextDouble();
+
+    double totalArea = 0;
+    if i<no.Walls
+
+    {
+        public static void main (String[]args){
         Scanner input1 = new Scanner(System.in);
         System.out.println("Enter Width(Meters)");
         double width = (double) input1.nextDouble();
@@ -16,11 +26,14 @@ public class Main {
         double area = width * height;
         System.out.println("Area of Wall is " + area + "Meters^2");
 
+        double totalArea = totalArea + area;
+    }
+
         Scanner input3 = new Scanner(System.in);
         System.out.println("Enter Area Per Tin");
         double areaPerTin = (double) input3.nextDouble();
 
-        int noOfTins =(int) ((Math.round(area/areaPerTin)) + Math.ceil((area%areaPerTin)/areaPerTin));
+        int noOfTins = (int) ((Math.round(area / areaPerTin)) + Math.ceil((area % areaPerTin) / areaPerTin));
         System.out.println("The number of tin(s) required is " + noOfTins);
     }
 }
